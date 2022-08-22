77062492cf0830dc4cd556f9b705928d081b32e2
Simone Pasquini <simo.pp07@gmail.com>
2022-08-22 12:07:28 +0200

    Implement operation on vector_2d

--- objects.cpp
+++ objects.cpp
@@ -6,27 +6,34 @@ double vector_2d::ycomp() const { return m_y; }
 
 double vector_2d::norm() const { return sqrt(m_x * m_x + m_y * m_y); }
 
-vector_2d vector_2d::operator+(vector_2d const &v) {
-  vector_2d res(this->m_x + v.m_x, this->m_y + v.m_y);
-  return res;
+vector_2d& vector_2d::operator+=(vector_2d const& v) {
+  m_x += v.m_x;
+  m_y += v.m_y;
+  return *this;
 }
 
-vector_2d vector_2d::operator-(vector_2d const &v) {
-  vector_2d res(this->m_x - v.m_x, this->m_y - v.m_y);
-  return res;
-}
-
-vector_2d vector_2d::operator*(double k) {
-  vector_2d res(this->m_x * k, this->m_y * k);
-  return res;
-}
-
-vector_2d vector_2d::operator+=(vector_2d const &v) {
-  this->m_x += v.m_x;
-  this->m_y += v.m_y;
+vector_2d& vector_2d::operator*=(vector_2d const& v) {
+  m_x *= v.m_x;
+  m_y *= v.m_y;
   return *this;
 }
 
 void vector_2d::setx(double x) { this->m_x = x; }
 
 void vector_2d::sety(double y) { this->m_y = y; }
+
+vector_2d operator-(vector_2d const& v) {
+  return {-v.xcomp(), -v.ycomp()};
+}
+
+vector_2d operator+(vector_2d const& l, vector_2d const& r) {
+  auto result{l};
+  return result += r;
+}
+
+vector_2d operator-(vector_2d const& l, vector_2d const& r) { return l + (-r); }
+
+vector_2d operator*(vector_2d const& l, vector_2d const& r) {
+  auto result{l};
+  return result *= r;
+}
--- objects.hpp
+++ objects.hpp
@@ -3,15 +3,12 @@
 
 #include <cmath>
 
-#define PI 3.141592
-
 // The class vector_2d defines a two dimensional vector useful to take care of
 // the position and velocity of the boids, the methods included allow to get the
 // private members of the class and apply basic math operation between vectors
 class vector_2d {
- private:
-  double m_x{};
-  double m_y{};
+  double m_x;
+  double m_y;
 
  public:
   vector_2d(double x = 0., double y = 0.) : m_x{x}, m_y{y} {}
@@ -22,18 +19,24 @@ class vector_2d {
 
   double norm() const;
 
-  vector_2d operator+(vector_2d const &v);
+  vector_2d& operator+=(vector_2d const& v);
 
-  vector_2d operator-(vector_2d const &v);
-
-  vector_2d operator*(double k);
-
-  vector_2d operator+=(vector_2d const &v);
+  vector_2d& operator*=(vector_2d const& v);
 
   void setx(double x);
 
   void sety(double y);
 };
+
+// Symmetric operators (e.g. operator+) are implemented in terms of member
+// operator (e.g. operator+=)
+vector_2d operator-(vector_2d const& v);
+
+vector_2d operator+(vector_2d const& l, vector_2d const& r);
+
+vector_2d operator-(vector_2d const& l, vector_2d const& r);
+
+vector_2d operator*(vector_2d const& l, vector_2d const& r);
 
 // Boid and predator struct are basically the same but are defined separatly
 // allowing an easier management of the two
